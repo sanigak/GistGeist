adea5cb3f3c425257010cc18931fc1e413165b58
Keith Saniga <sanigak@outlook.com>
2019-03-04 10:51:32 -0500

    CNN  engine nearly functional

--- GistGeist/ArticleScraper.py
+++ GistGeist/ArticleScraper.py
@@ -13,7 +13,7 @@ def CNNFrontPageLinks():
      
     homeurl = 'http://www.cnn.com/us'
 
-    page = requests.get(homeurl)
+    page = requests.get(homeurl, verify = False)
     soup = BeautifulSoup(page.text, 'html.parser')
     list = soup.find_all('a')
 
@@ -25,15 +25,14 @@ def CNNFrontPageLinks():
 
         if "/politics/" in URLS or "/us/" in URLS:
             if "specials" not in URLS and "video" not in URLS:
+                URLS = "https://www.cnn.com" + URLS
                 outputList.append(URLS)
 
     homeurl = 'http://www.cnn.com/world'
 
-    page = requests.get(homeurl)
+    page = requests.get(homeurl, verify = False)
     soup = BeautifulSoup(page.text, 'html.parser')
     list = soup.find_all('a')
-
-    outputList = []
       
     for item in list:
 
@@ -42,6 +41,7 @@ def CNNFrontPageLinks():
         if "world" in URLS or "asia" in URLS or "middleeast" in URLS or "africa" in URLS or "americas" in URLS or "europe" in URLS:
             if (URLS.count('-') > 3):
                 if "video" not in URLS:
+                    URLS = "https://www.cnn.com" + URLS
                     outputList.append(URLS)
         
 
@@ -123,17 +123,27 @@ def CNNArticleToText(URL):
     page = requests.get(homeurl, verify = False)
     soup = BeautifulSoup(page.text, 'html.parser')
     list = soup.find_all("div")
+    headline = soup.find_all('h1')
+    iterator = 0
+
+    for title in headline:
+        stringy = title.string
+        text_file.write(stringy)
+        text_file.write("\n")
 
     for item in list:
-        #print(item)
-        #print()
         temp = str(item.contents)
         if "zn-body" in temp:
             temp = re.sub('<[^<]+?>', '', temp)
             if "(CNN)" in temp:
-                print(temp)
 
-            print()
+                if (iterator == 4):
+                    text_file.write(temp)
+                iterator += 1
+
+
+    
+
     pass
 
 #Parses a Fox article into a dict of words and their frequencies
@@ -236,6 +246,40 @@ def getTitle():
     file_object = open("output.txt", "r")
     return file_object.readline()
 
+#Sub-engine for the CNN-related tasks -- BROKEN
+def CNNEngine():
+    myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+    mydb = myclient["GistGeist"]
+    mycol = mydb["CNNArticles"]
+
+    CNNlinks = CNNFrontPageLinks()
+
+    for link in CNNlinks:
+
+        if link != None:
+
+            print(link)
+            FoxArticleToText(link)
+            cleanupTXT()
+
+        
+            title = getTitle()
+            contents = re.findall(r'\w+', open('output.txt').read().lower())
+            frequency = Counter(contents)
+            x = datetime.datetime.now()
+            date = x.strftime("%x")
+        
+
+            mydict = {
+                "title": title,
+                "contents": frequency,
+                "date": date
+                }
+            
+            mycol.insert_one(mydict)
+
+    pass
+
 #Sub-engine for the Fox-related tasks
 def FoxEngine():
     myclient = pymongo.MongoClient("mongodb://localhost:27017/")
@@ -304,14 +348,13 @@ def BBCEngine():
 
     pass
 
-
 #Executes article scraping
 def Engine():
 
+    
     FoxEngine()
     BBCEngine()
 
     pass
 
 Engine()
-
