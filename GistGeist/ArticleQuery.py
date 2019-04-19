f4e866088cb12a64116bbf927a587da8b5eff1f5
Keith Saniga <sanigak@outlook.com>
2019-04-19 11:04:53 -0400

    made a couple variations on plotGeneration

--- GistGeist/ArticleQuery.py
+++ GistGeist/ArticleQuery.py
@@ -38,6 +38,7 @@ def numArticles(date, col):
     
 
 
+
 #Set of methods which take in the results of a level 1 query to extract specific info
 #I refer to this as a level 2 query
 
@@ -103,7 +104,8 @@ def wordFreqContents(contentsList, word):
 
 
 
-def plotGeneration(word, col, startDate, endDate):
+#Plots a single word from a single collection over date range
+def plotGenerationSingle(word, col, startDate, endDate):
 
     dates = dateRange(startDate, endDate)
 
@@ -130,6 +132,102 @@ def plotGeneration(word, col, startDate, endDate):
     plt.plot(simpDates, plotList)
     plt.show()
 
+#Plots a pair of words from a single collection over date range
+def plotGenerationDouble(word1, word2, col, startDate, endDate):
+
+    dates = dateRange(startDate, endDate)
+
+
+    simpDates = []
+    plotList1 = []
+    plotList2 = []
+
+    for date in dates:
+
+        simpDates.append(date[:-3])
+
+
+        query = returnDate(date,col) 
+        num = numArticles(date,col)
+        content = returnContents(query)
+        freq1 = wordFreqContents(content,word1)
+        freq2 = wordFreqContents(content,word2)
+        
+        if num > 0:
+            freq1 = freq1/num
+        if num > 0:
+            freq2 = freq2/num
+        
+
+        
+        
+        plotList1.append(freq1)
+        plotList2.append(freq2)
+
+
+    plt.plot(simpDates, plotList1, label = word1)
+    plt.plot(simpDates, plotList2, label = word2)
+    plt.legend()
+    plt.show()
+
+#Plots a single word from all 4 collections over date range
+def plotGenerationAllCol(word, startDate, endDate):
+
+    dates = dateRange(startDate, endDate)
+
+    simpDates = []
+    plotListFox = []
+    plotListCNN = []
+    plotListBBC = []
+    plotListAJ = []
+
+    for date in dates:
+
+        simpDates.append(date[:-3])
+        
+        queryFox = returnDate(date,FOXcol) 
+        numFox = numArticles(date,FOXcol)
+        contentFox = returnContents(queryFox)
+        freqFox = wordFreqContents(contentFox,word)
+        if numFox > 0:
+            freqFox = freqFox/numFox
+        plotListFox.append(freqFox)
+
+        queryCNN = returnDate(date,CNNcol) 
+        numCNN = numArticles(date,CNNcol)
+        contentCNN = returnContents(queryCNN)
+        freqCNN = wordFreqContents(contentCNN,word)
+        if numCNN > 0:
+            freqCNN = freqCNN/numCNN
+        plotListCNN.append(freqCNN)
+
+        queryBBC = returnDate(date,BBCcol) 
+        numBBC = numArticles(date,BBCcol)
+        contentBBC = returnContents(queryBBC)
+        freqBBC = wordFreqContents(contentBBC,word)
+        if numBBC > 0:
+            freqBBC = freqBBC/numBBC
+        plotListBBC.append(freqBBC)
+
+        queryAJ = returnDate(date,AJcol) 
+        numAJ = numArticles(date,AJcol)
+        contentAJ = returnContents(queryAJ)
+        freqAJ = wordFreqContents(contentAJ,word)
+        if numAJ > 0:
+            freqAJ = freqAJ/numAJ
+        plotListAJ.append(freqAJ)
+
+
+    plt.plot(simpDates, plotListFox, label = "Fox")
+    plt.plot(simpDates, plotListCNN, label = "CNN")
+    plt.plot(simpDates, plotListBBC, label = "BBC")
+    plt.plot(simpDates, plotListAJ, label = "AJ")
+    plt.legend()
+    plt.show()
+
+
+
+#Returns a list of frequencies of a given word from a given collection over a date range
 def returnWordFreq(word, col, startDate, endDate):
     dates = dateRange(startDate, endDate)
 
@@ -144,5 +242,4 @@ def returnWordFreq(word, col, startDate, endDate):
             freq = freq/num
         outputList.append(freq)
     return outputList
-
 
